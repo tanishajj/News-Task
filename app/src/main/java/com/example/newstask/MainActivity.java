04956b2f0ed834ea58314f4079e58b4fb00190cb
vinodpanchal13 <vinod.panchal@kodytechnolab.com>
2021-06-24 13:27:44 +0530

    Commit

--- app/src/main/java/com/example/newstask/MainActivity.java
+++ app/src/main/java/com/example/newstask/MainActivity.java
@@ -4,11 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.newstask.model.Article;
+import com.example.newstask.rest.ApiClient;
+import com.example.newstask.rest.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String API_KEY = "6ad69394f70947a9f4a83b37864 9a0c8e375ada4198a26f7a52638c4b78";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        Call<Response> responseCall = apiService.getLatestNews("google-news",API_KEY);
+
+        responseCall.enqueue(new Callback<Response>() {
+            @Override
+            public void onResponse(Call<Response> call, Response<Response> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<Response> call, Throwable t) {
+
+            }
+        });
+
     }
 }
